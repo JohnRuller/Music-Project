292678786d3f6ee1663cd869800ac7148bce1444
JohnRuller <john.ruller@gmail.com>
2013-11-07 21:52:33 -0600

    Initial Commit

---
+++ Music Project/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Music Project
+//
+//  Created by John Ruller on 2013-11-07.
+//  Copyright (c) 2013 John Ruller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Music ProjectTests/Music_ProjectTests.m
@@ -0,0 +1,34 @@
+//
+//  Music_ProjectTests.m
+//  Music ProjectTests
+//
+//  Created by John Ruller on 2013-11-07.
+//  Copyright (c) 2013 John Ruller. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Music_ProjectTests : XCTestCase
+
+@end
+
+@implementation Music_ProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
