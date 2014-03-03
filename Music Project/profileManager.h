5ae12f826a2d3617e82f3405977d32779ca614c2
JohnRuller <john.ruller@gmail.com>
2014-03-03 13:54:47 -0600

    Added profileManager class

---
+++ Music Project/profileManager.h
@@ -0,0 +1,15 @@
+//
+//  profileManager.h
+//  Music Project
+//
+//  Created by John Ruller on 2014-03-03.
+//  Copyright (c) 2014 John Ruller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface profileManager : NSObject {
+    
+}
+
+@end
---
+++ Music Project/profileManager.m
@@ -0,0 +1,13 @@
+//
+//  profileManager.m
+//  Music Project
+//
+//  Created by John Ruller on 2014-03-03.
+//  Copyright (c) 2014 John Ruller. All rights reserved.
+//
+
+#import "profileManager.h"
+
+@implementation profileManager
+
+@end
